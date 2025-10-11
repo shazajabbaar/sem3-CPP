6bfb3e53654383c3b769c03c0cf71d601351c1ed
shazajabbaar <shazajabbar02@gmail.com>
2025-10-11 17:54:50 +0300

    Create car_program_2.cpp

---
+++ car_program_2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Car {
+    string brand;
+    string model;
+    int year;
+
+public:
+    void readData() {
+        cout << "Enter brand: ";
+        cin >> brand;
+        cout << "Enter model: ";
+        cin >> model;
+        cout << "Enter year: ";
+        cin >> year;
+    }
+
+    void displayData() {
+        cout << "\nCar Details:\n";
+        cout << "Brand: " << brand << endl;
+        cout << "Model: " << model << endl;
+        cout << "Year: " << year << endl;
+    }
+};
+
+int main() {
+    Car car1, car2, car3;
+
+    cout << "Enter details for Car 1:\n";
+    car1.readData();
+
+    cout << "\nEnter details for Car 2:\n";
+    car2.readData();
+
+    cout << "\nEnter details for Car 3:\n";
+    car3.readData();
+
+    cout << "\n--- Displaying Car Details ---";
+    car1.displayData();
+    car2.displayData();
+    car3.displayData();
+
+    return 0;
+}
